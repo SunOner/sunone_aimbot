684787a1247538f27bd9ec08cd914b131d4b2a1a
SunOne <bozn1.9@mail.ru>
2023-10-16 14:43:17 +0300

    update

--- utils/rename_train_images.py
+++
@@ -1,7 +0,0 @@
-import os
-count = 0
-for filename in os.listdir("C:/train_data/"):
-    new_file_name = "13.10.2023_1_{0}.{1}".format(count, filename.split('.')[-1])
-    os.rename("C:/train_data/{0}".format(filename), "C:/train_data/{0}".format(new_file_name))
-    count = count + 1
-print("Done!")
